f0cbce97f636d23999f4aa00ea26d240432d1dea
Matt Hagner <web@matthagner.com>
2018-08-02 08:08:39 -0500

    Set up drum pad data

---
+++ src/data.js
@@ -0,0 +1,58 @@
+const pads = [
+  {
+    id: "Q",
+    keyCode: 81,
+    name: "Kick",
+    src: "https://freesound.org/data/previews/344/344761_1676145-lq.mp3"
+  },
+  {
+    id: "W",
+    keyCode: 87,
+    name: "Snare",
+    src: "https://freesound.org/data/previews/212/212208_2381150-lq.mp3"
+  },
+  {
+    id: "E",
+    keyCode: 69,
+    name: "Closed-HH",
+    src: "https://freesound.org/data/previews/269/269720_4965320-lq.mp3"
+  },
+  {
+    id: "A",
+    keyCode: 65,
+    name: "Closed-HH",
+    src: "https://freesound.org/data/previews/219/219613_3008343-lq.mp3"
+  },
+  {
+    id: "S",
+    keyCode: 83,
+    name: "Tonk",
+    src: "https://freesound.org/data/previews/191/191635_2394245-lq.mp3"
+  },
+  {
+    id: "D",
+    keyCode: 68,
+    name: "Ride",
+    src: "https://freesound.org/data/previews/33/33610_294523-lq.mp3"
+  },
+  {
+    id: "Z",
+    keyCode: 90,
+    name: "Clap",
+    src: "https://freesound.org/data/previews/183/183102_2394245-lq.mp3"
+  },
+  {
+    id: "X",
+    keyCode: 88,
+    name: "Clave",
+    src: "https://freesound.org/data/previews/75/75836_260058-lq.mp3"
+  },
+  {
+    id: "C",
+    keyCode: 67,
+    name: "Record-Scratch",
+    src: "https://freesound.org/data/previews/257/257777_4157918-lq.mp3"
+  }
+];
+
+export default pads;
