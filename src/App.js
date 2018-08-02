3dc4d1f0d929a6fe4e5cc87921b37f7518e36b34
Matt Hagner <web@matthagner.com>
2018-08-02 08:07:08 -0500

    Flesh out App component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import React from "react";
 
+import DrumMachine from "./components/drum-machine";
+import Display from "./components/display";
+import Title from "./components/title";
+import Footer from "./components/footer";
+import { DrumPads } from "./components/drum-pad";
+
+import Pads from "./data";
+
 const App = () => (
-  <div>
-    <div id="drum-machine">
-      <div id="display" />
-      <div className="drum-pad">
-        <audio className="clip" />
+  <DrumMachine
+    pads={Pads}
+    render={({ currentName, handleClick }) => (
+      <div id="drum-machine">
+        <Title />
+        <Display text={currentName} />
+        <DrumPads triggerSound={handleClick} />
+        <Footer />
       </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-      <div className="drum-pad">
-        <audio className="clip" />
-      </div>
-    </div>
-  </div>
+    )}
+  />
 );
 
 export default App;
