09d4d87ff55afa2d26f3d55cabf805fb82805446
Matt Hagner <web@matthagner.com>
2018-08-02 08:11:01 -0500

    Fulfill footer and title components

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <div id="footer">
+    <p>Made and Designed by Matt Hagner</p>
+    <p>Made with React &amp; Emotion</p>
+    <p>Tested with Jest &amp; React Testing Library</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/title.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Title = () => <h1>FCC DRUM MACHINE</h1>;
+
+export default Title;
